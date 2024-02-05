fb6a902a6a7a46d5b11d48749715b8d0abf4c6a8
mpoplavkov <mihail.poplavkov@gmail.com>
2024-02-05 13:33:22 +0000

    build: always rerun codegen

--- packages/fuelet_smart_contract_wallet/native/build.rs
+++ packages/fuelet_smart_contract_wallet/native/build.rs
@@ -10,7 +10,7 @@ const MACOS_C_OUTPUT_DIR: &str = "../../flutter_fuelet_smart_contract_wallet/mac
 
 fn main() {
     // Tell Cargo that if the input Rust code changes, rerun this build script
-    println!("cargo:rerun-if-changed={}", RUST_INPUT);
+    // println!("cargo:rerun-if-changed={}", RUST_INPUT);
 
     // Options for frb_codegen
     let raw_opts = RawOpts {
