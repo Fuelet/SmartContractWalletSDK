a8d886b40e38f3a4425f9c4b28244b98cd560da5
mpoplavkov <mihail.poplavkov@gmail.com>
2024-02-06 17:02:46 +0000

    feat: basic example

--- packages/flutter_fuelet_smart_contract_wallet/example/lib/main.dart
+++ packages/flutter_fuelet_smart_contract_wallet/example/lib/main.dart
@@ -1,74 +1,94 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter_fuelet_smart_contract_wallet/flutter_fuelet_smart_contract_wallet.dart' as flutter_fuelet_smart_contract_wallet;
+import 'package:flutter/material.dart';
+import 'package:flutter_fuelet_smart_contract_wallet/wallet/smart_contract_wallet.dart';
+
+const _betaApiUrl = 'https://beta-5.fuel.network';
+const _testWalletPrivateKey =
+    'e5e05a4ab2919dc01b97c90a48853fd4dfbd204e92e44327375702ab09bb184e';
+const _stubR1PublicKey =
+    '0xfe1177573a47310756f27ef25c7229b5da8d5109a7258b297f23a6768c78cb40';
+const _notGeneratedTitle = 'not generated yet';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SmartContractWalletUsageExample());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class SmartContractWalletUsageExample extends StatefulWidget {
+  const SmartContractWalletUsageExample({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<SmartContractWalletUsageExample> createState() =>
+      _SmartContractWalletUsageExampleState();
 }
 
-class _MyAppState extends State<MyApp> {
-  late int sumResult;
-  late Future<int> sumAsyncResult;
+const _textStyle = TextStyle(fontSize: 25);
+
+List<Widget> _commonText(String text) {
+  return [
+    SelectableText(
+      text,
+      style: _textStyle,
+      textAlign: TextAlign.left,
+    ),
+    const SizedBox(height: 8),
+  ];
+}
+
+List<Widget> _commonButton(String text, VoidCallback? onPressed) {
+  return [
+    OutlinedButton(
+      onPressed: onPressed,
+      child: Text(text),
+    ),
+    const SizedBox(height: 8),
+  ];
+}
+
+class _SmartContractWalletUsageExampleState
+    extends State<SmartContractWalletUsageExample> {
+  String _r1PublicKey = _stubR1PublicKey;
+  String _recoveryWalletPrivateKey = _testWalletPrivateKey;
+  String _predicateAddress = _notGeneratedTitle;
 
   @override
   void initState() {
     super.initState();
-    sumResult = flutter_fuelet_smart_contract_wallet.sum(1, 2);
-    sumAsyncResult = flutter_fuelet_smart_contract_wallet.sumAsync(3, 4);
   }
 
   @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
     const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Native Packages'),
+          title: const Text('Fuelet smart contract wallet example'),
         ),
         body: SingleChildScrollView(
           child: Container(
             padding: const EdgeInsets.all(10),
             child: Column(
               children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                Text(
-                  'sum(1, 2) = $sumResult',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                FutureBuilder<int>(
-                  future: sumAsyncResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue =
-                        (value.hasData) ? value.data : 'loading';
-                    return Text(
-                      'await sumAsync(3, 4) = $displayValue',
-                      style: textStyle,
-                      textAlign: TextAlign.center,
-                    );
-                  },
-                ),
+                ..._commonText('r1PublicKey: $_r1PublicKey'),
+                ..._commonText(
+                    'recoveryWalletPrivateKey: $_recoveryWalletPrivateKey'),
+                ..._commonText('predicateAddress: $_predicateAddress'),
+                ..._commonButton('Connect', _connectToWallet),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> _connectToWallet() async {
+    final wallet = await SmartContractWallet.connect(
+        networkUrl: _betaApiUrl,
+        r1PublicKey: _r1PublicKey,
+        recoveryWalletPrivateKey: _recoveryWalletPrivateKey);
+    setState(() {
+      _predicateAddress = wallet.predicateBech32Address;
+    });
   }
 }
